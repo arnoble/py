2fee8aad3a44d900ce5c3b5b8472cb5d0e7bd55a
Art Noble <arnoble@btinternet.com>
2016-01-13 20:59:35 +0000

    Working DERP. Next is to check DEIP calcs

--- test/src/Solver.py
+++ test/src/Solver.py
@@ -1,6 +1,13 @@
+import sys
 import math
 import pyodbc
-
+import mysql.connector
+import datetime
+#
+# date from data    previousDate >= datetime.date(2015,11,10)
+#
+#
+#
 class Solver:
     def demo(self):
         while True:
@@ -20,51 +27,83 @@ class Solver:
 
 class DBconn:
     def connect(self):
-        return pyodbc.connect('DSN=newSp;PWD=ragtinmor')
-
-
+        ## return pyodbc.connect('DSN=newSp;PWD=ragtinmor')
+        #return pyodbc.connect('DRIVER={MySQL ODBC 3.51 Driver};Login Prompt=False;User ID=root;Password=ragtinmor;Data Source=localhost;Database=sp')
+        return mysql.connector.connect(user='anoble', password='Ragtin_Mor14_Lucian',
+                              host='166.63.0.149',
+                              database='sp')
 #####
 # DERP index
 #
-# Clock is driven by daily arrival or productPrices
-# ... if a new day arrives, compute new portValue as at last close and calc the index
-# ... then accumulate any coupons for the current positions
-# ... then do any new trades on/before thisDate
+#  Clock is driven by arrival (not necessarily daily) of a date-ordered price for 1 product
+# ... if a new day arrives "close off the books" for precedingDate
+#    ... indexReturn is mean(positionReturns) which implies daily rebalancing to equal weights
+#        ... unrealistic, but close to the complicated alternative of BUYS/SELLS on trade dates
+#            ... minimisation problem:  min(bidAskCosts = deltaTrades*bidAsks) subject to
+#                ... (oldPositions + deltaTrades)*ASKS equally weighted = 1/N * sum((oldPositions + deltaTrades)*ASKS)
+#                ... (oldPositions + deltaTrades)> 0
+#
+#    ... accumulate any coupons ON_OR_BEFORE precedingDate for previousPositions
+#    ... do any trades BEFORE precedingDate (trade.Date could fall between productPrices.Dates)
+#        ... trades assumed executed on precedingDate
+#        ... raise ERROR if no pricing
+#        ... assert positions either ZERO or ONE
+#        ... productReturn = returnSinceLastUsingMIDS - midToBidIfSold - midToAskIfBought + coupons
+#    ... compute portValue as at precedingDate and update the index
+#
+# ... otherwise just record bid/ask for this productPrice
 #
 #####
+
+#
+# functions
+#
+
 #
 #init
 #
-userId = 137
 cnxn   = DBconn().connect()
-cursor = cnxn.cursor()
+cursor = cnxn.cursor(named_tuple=True)  # or user dictionary=True
+for a in sys.argv:
+    print("Argv:",a)
+if len(sys.argv)<2:
+    print("Usage: loginEmail")
+    exit()
+q = "select UserId  from user where email = '"+sys.argv[1]+"'"
+cursor.execute(q)
+userId = cursor.fetchone()
+if not userId:
+    print("NO user with that email")
+    exit()
+userId = userId.UserId
+
+
+# userId = 137  is DERP@cubeinvesting.com
+
 
 #
-# get trades
+# get trades in date,Position order so that SELLS come before BUYS
 #
 tradeIndex = 0
-cursor.execute("""
-        select Date,t.ProductId,100*Position/IssuePrice Position
-        from trade t join product using (productid)
-        where InvestorId = ?
-        order by Date,ProductId
-        """,[userId])
+q = "select * from trade where investorid=" + format(userId) + " order by Date,Position,ProductId"
+cursor.execute(q)
 trades = cursor.fetchall()
 if not trades:
     print("NO trade DATA")
     exit()
+
+#for (trade_date,pos) in cursor:
+#  print("{}, was hired on {:%d %b %Y}".format(
+#    pos, trade_date))
+
 firstDate = trades[0].Date
 numTrades = len(trades)
 #
 # get productPrices
 #
 productPriceIndex = 0
-cursor.execute("""
-        select pp.*
-        from productprices pp join trade using (productid)
-        where InvestorId = ? and pp.Date >= ?
-        order by Date,ProductId
-        """,[userId,firstDate])
+q = "select pp.* from productprices pp join trade using (productid) where InvestorId = "+format(userId)+" and pp.Date >= '"+format(firstDate)+"' order by Date,ProductId"
+cursor.execute(q)
 productPrices = cursor.fetchall()
 if not productPrices:
     print("NO price DATA")
@@ -74,12 +113,8 @@ if not productPrices:
 # get coupons
 #
 couponIndex = 0
-cursor.execute("""
-        select pc.*
-        from productcoupons pc join trade using (productid)
-        where InvestorId = ? and pc.Date > ?
-        order by Date,ProductId
-        """,[userId,firstDate])
+q = "select pc.* from productcoupons pc join trade using (productid) where InvestorId = "+format(userId)+" and pc.Date >= '"+format(firstDate)+"' order by Date,ProductId"
+cursor.execute(q)
 coupons = cursor.fetchall()
 if not coupons:
     print("NO coupon DATA")
@@ -91,75 +126,138 @@ numCoupons = len(coupons)
 #
 # compute index
 #
-currentBids       = {}
-currentAsks       = {}
-currentPositions  = {}
-sumCoupons        = 0.0
-indexValue        = 1.0
-portValue         = 0.0
-previousDate      = firstDate
-previousPortValue = 0.0
+currentBids          = {}
+currentAsks          = {}
+previousBids         = {}
+previousAsks         = {}
+currentPositions     = {}
+previousPositions    = {}
+sumCoupons           = {}
+indexValue           = 1.0
+previousDate         = firstDate
+isFirstDate          = True
 #
-#  Clock is driven by daily arrival or productPrices
+#  Clock is driven by arrival (not necessarily daily) of a date-ordered price for 1 product
 #
 for productPrice in productPrices:
-    # init
     thisDate = productPrice.Date
     #
-    # ... if a new day arrives, compute new portValue as at last close and calc the index
+    # ... if a new day arrives, compute portValue as at previousDate and update the index
+    #    ... indexReturn is mean(positionReturns) which implies daily rebalancing to equal weights
     #
     if thisDate != previousDate:
-        # add any new trades prior to thisDate
-        while tradeIndex < numTrades and trades[tradeIndex].Date < thisDate:
+        # ... accumulate any coupons ON_OR_BEFORE previousDate for previousPositions
+        while couponIndex < numCoupons and [couponIndex].Date <= previousDate:
+            thisPid     = coupons[couponIndex].ProductId
+            if thisPid in previousPositions:
+                thisCoupon  = previousPositions[thisPid] * coupons[couponIndex].Amount
+                if thisPid not in sumCoupons:
+                    sumCoupons[thisPid] = thisCoupon
+                else:
+                    sumCoupons[thisPid] += thisCoupon
+            couponIndex = couponIndex + 1
+
+        #    ... reflect in currentPositions any trades BEFORE precedingDate (trade.Date could fall between productPrices.Dates)
+        while tradeIndex < numTrades and (trades[tradeIndex].Date < previousDate or (trades[tradeIndex].Date <= previousDate and trades[tradeIndex].Position<0.0)):
+            tradeDate     = trades[tradeIndex].Date
             tradePid      = trades[tradeIndex].ProductId
             tradePosition = trades[tradeIndex].Position
-            # ...init in case trade date is before firstPriceDate
-            if tradePid not in currentBids:
-                currentBids[tradePid] = 0.0
-            if tradePid not in currentAsks:
-                currentAsks[tradePid] = 0.0
-            # ... init new positions
-            if tradePosition > 0:
-                previousPortValue += tradePosition * currentAsks[tradePid]
+            #        ... trades assumed executed on precedingDate
+            #        ... raise ERROR if no pricing
+            if tradePid not in previousBids or  tradePid not in previousAsks:
+                print("No prices for",tradePid,"on",tradeDate)
+                exit()
+            #        ... assert positions either ZERO or ONE
+            if tradePid in previousPositions:
+                currentPosition = previousPositions[tradePid]
             else:
-                previousPortValue += tradePosition * currentBids[tradePid]
-            if tradePid in currentPositions:
-                currentPositions[tradePid]  += tradePosition
-            else:
-                currentPositions[tradePid]   = tradePosition
+                currentPosition = 0.0
+            newPosition = currentPosition + tradePosition
+            if newPosition>1 or newPosition<0:
+                print("Position too large",tradePid,"on",tradeDate)
+                exit()
+            # ... update positions
+            currentPositions[tradePid]   = newPosition
             tradeIndex = tradeIndex + 1
 
 
-        # calc portValue
-        portValue = sumCoupons
+        # ... compute portReturn (equal weight, continually rebalanced) as at precedingDate and update the index
+        portReturn   = 0.0
+        numPositions = 0
         for pid,pos in currentPositions.items():
-            portValue += currentBids[pid] * pos
+            if pid not in previousPositions:
+                previousPosition = 0.0
+            else:
+                previousPosition = previousPositions[pid]
+            positionChange = pos - previousPosition
+            previousMid    = (previousBids[pid] + previousAsks[pid])/2.0
+            currentMid     = (currentBids [pid] + currentAsks [pid])/2.0
+            if pid not in sumCoupons:
+                thisCoupon = 0.0
+            else:
+                thisCoupon = sumCoupons[pid]
+            # ... productReturn = returnSinceLastUsingMIDS - midToBidIfSold - midToAskIfBought + coupons
+            if positionChange>0:
+                # crap
+                # thisReturn  = (currentMid       + thisCoupon)/previousAsks[pid]
+                thisReturn  = (currentBids[pid]       + thisCoupon)/previousBids[pid]
+                numPositions += 1
+            elif positionChange<0:
+                # crap
+                # thisReturn  = (currentBids[pid] + thisCoupon)/previousMid
+                thisReturn  = (currentBids[pid]       + thisCoupon)/previousBids[pid]
+                numPositions += 1
+            elif pos>0:
+                # crap
+                # thisReturn  = (currentMid       + thisCoupon)/previousMid
+                thisReturn  = (currentBids[pid]       + thisCoupon)/previousBids[pid]
+                numPositions += 1
+            else:
+                thisReturn  = 0.0
+
+
+            portReturn  += thisReturn                #  previousDate >= datetime.date(2016,1,8)
         # calc index
-        indexValue  *= portValue/previousPortValue
-        print("Index:",previousDate,indexValue)
+        if isFirstDate:
+            isFirstDate       = False
+        else:
+            portReturn  /= numPositions
+            indexValue  *= portReturn
+
         #
         # save new index value
         #
+        print("Index:",previousDate,indexValue)
         # cursor.execute("insert into products(id, name) values (?, ?)", 'pyodbc', 'awesome library')
         # cnxn.commit()
         #
+
         # init for next time
+        # ... track positions
+        for pid,pos in currentPositions.items():
+            previousPositions[pid] = currentPositions[pid]
+        # ... zero coupons
+        for x in sumCoupons:
+            sumCoupons[x] = 0.0
+        # ... track bids, asks
+        for pid,x in currentBids.items():
+            previousBids[pid] = x
+        for pid,x in currentAsks.items():
+            previousAsks[pid] = x
+        # ... track date
         previousDate       = thisDate
-        previousPortValue  = portValue
 
 
-
-    # accumulate any new coupons up to this date for existing positions
-    # DOME: ccy conversion
-    while couponIndex< numCoupons and [couponIndex].Date <= thisDate:
-        sumCoupons += currentPositions[thisPid] * coupons[couponIndex].Amount
-        couponIndex = couponIndex + 1
-
-
-    # update for this price
+    # ... otherwise just record bid/ask for this productId
     thisPid              = productPrice.ProductId
-    currentBids[thisPid] = productPrice.Bid
-    currentAsks[thisPid] = productPrice.Ask
+    if productPrice.Bid > 0.0:
+        currentBids[thisPid] = productPrice.Bid
+        if thisPid not in previousBids:
+            previousBids[thisPid] = productPrice.Bid
+    if productPrice.Ask > 0.0:
+        currentAsks[thisPid] = productPrice.Ask
+        if thisPid not in previousAsks:
+            previousAsks[thisPid] = productPrice.Ask
 
 
     #
