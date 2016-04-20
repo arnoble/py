3313fb69e4af472925ffe1d856f82967360ba36c
Art Noble <arnoble@btinternet.com>
2016-04-20 13:42:24 +0100

    Portfolio weights implemented

--- test/src/Solver.py
+++ test/src/Solver.py
@@ -40,27 +40,43 @@ class DBconn:
         else:
             return mysql.connector.connect(user='root', password='ragtinmor', host='localhost', database='sp')
 #####
-# DERP index
+# OVERVIEW
 #
-#  Clock is driven by arrival (not necessarily daily) of a date-ordered price for 1 product
-# ... if a new day arrives "close off the books" for precedingDate
-#    ... indexReturn is mean(positionReturns) which implies daily rebalancing to equal weights
-#        ... unrealistic, but close to the complicated alternative of BUYS/SELLS on trade dates
-#            ... minimisation problem:  min(bidAskCosts = deltaTrades*bidAsks) subject to
-#                ... (oldPositions + deltaTrades)*ASKS equally weighted = 1/N * sum((oldPositions + deltaTrades)*ASKS)
-#                ... (oldPositions + deltaTrades)> 0
+#  0. DATA
+#       Position column of 'trade' table records trades; in any self-consistent units you like, since they will be normalised to wts() portfolio weights
 #
-#    ... accumulate any coupons ON_OR_BEFORE precedingDate for previousPositions
-#    ... do any trades BEFORE precedingDate (trade.Date could fall between productPrices.Dates)
-#        ... trades assumed executed on precedingDate
-#        ... raise ERROR if no pricing
-#        ... assert positions either ZERO or ONE
-#        ... productReturn = returnSinceLastUsingMIDS - midToBidIfSold - midToAskIfBought + coupons
-#    ... compute portValue as at precedingDate and update the index
+#  1. INIT
+#       vector trades(),        date-ordered, selects all rows in 'trade' table for this 'InvestorId'
+#       vector productPrices(), date-ordered, selects all rows in 'productprices' table where ProductId's in 'trade' table for this 'InvestorId'
+#       vector coupons(),       date-ordered, selects all rows in 'productcoupons' table where ProductId's in 'trade' table for this 'InvestorId'
 #
-# ... otherwise just record bid/ask for this productPrice
+#       vector cumTrades(productId) stores cumulative Positions from trades() for each productId
+#
+#  2. LOOP through productPrices()
+#       3. IF NEW date 't' from productPrices(), clock "ticks" and we recalc everything at t-1
+#           3.1 update to t-1
+#               cumTrades(productId) from trades to t-1
+#               theseCoupons(productId) from coupons to t-1
+#               cashflow = sumOverProductId(previousUnits*theseCoupons)
+#               indexValue = sumOverProductId(previousUnits*previousMids)
+#               currentWeights(productid) recalculated from cumTrades, so as to sum to 1.0
+#               weightChanges  = currentWeights - previousWeights
+#
+#           3.2 IF absSum(WeightChanges) != 0.0   rebalance at t-1
+#               currentUnits = indexValue * currentWeights / previousMids  # trade at MIDs, and record transaction costs below in cashflows
+#               changeUnits  = currentUnits - previousUnits
+#               cashflow -= positive(changeUnits) * (previousAsk - previousMid)
+#               cashflow -= negative(changeUnits) * (previousMid - previousBid)
+#
+#           3.3 handle cashflows
+#               slippage      = 1.0 - cashflow/indexValue
+#               indexvalue    = indexvalue * slippage
+#               previousUnits = currentUnits * slippage
+#       4. update currentBid(productId),currentAsk(productId)
 #
 #####
+
+
 
 #
 # functions
@@ -68,6 +84,9 @@ class DBconn:
 
 #
 #init
+#
+# CautiousIncome@jbrearley.com spCloud
+# DERP@cubeinvesting.com    newSp
 #
 for a in sys.argv:
     print("Argv:",a)
@@ -90,12 +109,11 @@ if not userId:
 userId = userId.UserId
 
 
-# userId = 137  is DERP@cubeinvesting.com
-
 
 #
 # get trades in date,Position order so that SELLS come before BUYS
 #
+# vector trades(),        date-ordered, selects all rows in 'trade' table for this 'InvestorId'
 tradeIndex = 0
 q = "select * from trade where investorid=" + format(userId) + " order by Date,Position,ProductId"
 cursor.execute(q)
@@ -103,18 +121,29 @@ trades = cursor.fetchall()
 if not trades:
     print("NO trade DATA")
     exit()
-
 #for (trade_date,pos) in cursor:
-#  print("{}, was hired on {:%d %b %Y}".format(
+#  print("{}, was traded on {:%d %b %Y}".format(
 #    pos, trade_date))
 
 firstDate = trades[0].Date
 numTrades = len(trades)
+
+
 #
 # get productPrices
 #
+# DOME: if 'trade' has a ProductIsaPrice=1 row we should BEWARE as there could also be a row with the same (real product) ProductId but ProductIsaPrice=0
+q = "select * from trade t where InvestorId = "+format(userId)+" and t.ProductIsaPrice=0 and ProductId in (130)"
+cursor.execute(q)
+problemTrades = cursor.fetchall()
+if len(problemTrades)>0:
+    print("Illegal ProductId")
+    exit()
+#       ... leave for now as we only use UnderlyingId=130 for cash
+#       vector productPrices(), date-ordered, selects all rows in 'productprices' table where ProductId's in 'trade' table for this 'InvestorId'
 productPriceIndex = 0
-q = "select pp.* from productprices pp join trade using (productid) where InvestorId = "+format(userId)+" and pp.Date >= '"+format(firstDate)+"' order by Date,ProductId"
+q = "select pp.* from productprices pp join trade t using (productid) where InvestorId = "+format(userId)+" and pp.Date >= '"+format(firstDate)+"' and t.ProductIsaPrice=0 "
+q += "union select p.UnderlyingId ProductId,p.Date,p.Price Bid,p.Price Ask from prices p join trade t on (t.productid=p.underlyingid) where InvestorId = "+format(userId)+" and p.Date >= '"+format(firstDate)+"' and t.ProductIsaPrice=1 "+" order by Date,ProductId"
 cursor.execute(q)
 productPrices = cursor.fetchall()
 if not productPrices:
@@ -122,9 +151,12 @@ if not productPrices:
     exit()
 numPprices = len(productPrices)
 
+
+
 #
 # get coupons
 #
+#       vector coupons(),       date-ordered, selects all rows in 'productcoupons' table where ProductId's in 'trade' table for this 'InvestorId'
 couponIndex = 0
 q = "select distinct pc.ProductId,pc.Date,pc.Amount*IssuePrice Amount,pc.ccy from productcoupons pc join trade using (productid) join product using (productid) where InvestorId = "\
     +format(userId)+" and pc.Date >= '"+format(firstDate)+"' order by Date,ProductId"
@@ -134,12 +166,14 @@ if not coupons:
     print("NO coupon DATA")
 numCoupons = len(coupons)
 
+
 #
 # update strings
 #
 strategyIds          = {}
-strategyIds['DERP@cubeinvesting.com'] = 1007;
+strategyIds['DERP@cubeinvesting.com'] = 1007;           # userId = 137  is DERP@cubeinvesting.com
 strategyIds['DEIP@cubeinvesting.com'] = 1008;
+strategyIds['CautiousIncome@jbrearley.com'] = 1010;
 if userEmail not in strategyIds:
     print("NO strategy for",userEmail)
     exit()
@@ -151,44 +185,54 @@ cursor.execute("delete from indexstrategylevel   where indexstrategyid="+format(
 cursor.execute("delete from indexstrategyweights where indexstrategyid="+format(strategyId))
 cnxn.commit()
 
+
+
 #
 # compute index
 #
+cumTrades            = {} # (productId) stores cumulative Positions from trades() for each productId
 currentBids          = {}
 currentAsks          = {}
 previousBids         = {}
 previousAsks         = {}
 currentPositions     = {}
 previousPositions    = {}
+currentWeights       = {}
+previousWeights      = {}
+weightChanges        = {}
+previousUnits        = {}
+currentUnits         = {}
 sumCoupons           = {}
-indexValue           = 1.0
+indexValue           = 1000.0
 previousDate         = firstDate
 isFirstDate          = True
 #
-#  Clock is driven by arrival (not necessarily daily) of a date-ordered price for 1 product
+#  2. LOOP through productPrices()
 #
 ppCounter=0
 for productPrice in productPrices:
     ppCounter = ppCounter + 1
-    thisDate = productPrice.Date
-    #
-    # ... if a new day arrives, compute portValue as at previousDate and update the index
-    #    ... indexReturn is mean(positionReturns) which implies daily rebalancing to equal weights
-    #
+    thisDate  = productPrice.Date
+    #       3. IF NEW date 't' from productPrices(),or if this is the lasat date, clock "ticks" and we recalc everything at t-1
     if thisDate != previousDate or ppCounter == numPprices:
+        cashflow = 0.0
+        #           3.1 update to t-1
         # ... accumulate any coupons ON_OR_BEFORE previousDate for previousPositions
         while couponIndex < numCoupons and coupons[couponIndex].Date <= previousDate:
             thisPid     = coupons[couponIndex].ProductId
-            if thisPid in previousPositions:
-                thisCoupon  = previousPositions[thisPid] * coupons[couponIndex].Amount
+            if thisPid in previousUnits:
+                thisCoupon  = coupons[couponIndex].Amount
                 if thisPid not in sumCoupons:
                     sumCoupons[thisPid] = thisCoupon
                 else:
                     sumCoupons[thisPid] += thisCoupon
             couponIndex = couponIndex + 1
+        for pid,y in sumCoupons.items():
+            cashflow += previousUnits[pid] * sumCoupons[pid]
 
-        #    ... reflect in currentPositions any trades BEFORE precedingDate (trade.Date could fall between productPrices.Dates)
-        while tradeIndex < numTrades and (trades[tradeIndex].Date < previousDate or (trades[tradeIndex].Date <= previousDate and trades[tradeIndex].Position<0.0)):
+
+        #    ... reflect in currentPositions any trades ON_OR_BEFORE precedingDate
+        while tradeIndex < numTrades and trades[tradeIndex].Date <= previousDate:
             tradeDate     = trades[tradeIndex].Date
             tradePid      = trades[tradeIndex].ProductId
             tradePosition = trades[tradeIndex].Position
@@ -197,68 +241,73 @@ for productPrice in productPrices:
             if tradePid not in previousBids or  tradePid not in previousAsks:
                 print("No prices for",tradePid,"on",tradeDate)
                 exit()
-            #        ... assert positions either ZERO or ONE
+
             if tradePid in previousPositions:
                 currentPosition = previousPositions[tradePid]
             else:
                 currentPosition = 0.0
             newPosition = currentPosition + tradePosition
             if newPosition>1 or newPosition<0:
-                print("Position too large",tradePid,"on",tradeDate)
+                print("Illegal Position ",tradePid,"on",tradeDate)
                 exit()
             # ... update positions
             currentPositions[tradePid]   = newPosition
             tradeIndex = tradeIndex + 1
 
 
-        # ... compute portReturn (equal weight, continually rebalanced) as at precedingDate and update the index
-        portReturn   = 0.0
-        numPositions = 0
+        #               currentWeights(productid) recalculated from cumTrades, so as to sum to 1.0
+        sumPositions = 0.0
         for pid,pos in currentPositions.items():
-            if pid not in previousPositions:
-                previousPosition = 0.0
+            sumPositions += currentPositions[pid]
+        for pid,pos in currentPositions.items():
+            currentWeights[pid] = currentPositions[pid] / sumPositions
+
+        #               weightChanges  = currentWeights - previousWeights
+        absSumWeightChanges = 0.0
+        for pid,pos in currentPositions.items():
+            if pid not in previousWeights:
+                previousWeight = 0.0
             else:
-                previousPosition = previousPositions[pid]
-            positionChange = pos - previousPosition
-            previousMid    = (previousBids[pid] + previousAsks[pid])/2.0
-            currentMid     = (currentBids [pid] + currentAsks [pid])/2.0
-            if pid not in sumCoupons:
-                thisCoupon = 0.0
-            else:
-                thisCoupon = sumCoupons[pid]
-            # ... productReturn = returnSinceLastUsingMIDS - midToBidIfSold - midToAskIfBought + coupons
-            if positionChange>0:
-                # reference BIDs only
-                # thisReturn  = (currentBids[pid]       + thisCoupon)/previousBids[pid]
-                thisReturn  = (currentMid       + thisCoupon)/previousAsks[pid]
-                numPositions += 1
-                if debug:
-                    print("   BUY ProductId:",pid,"Return:",thisReturn,"PreviousAsk:",previousAsks[pid],"CurrentMid:",currentMid,"Coupons:",thisCoupon)
-            elif positionChange<0:
-                # reference BIDs only
-                # thisReturn  = (currentBids[pid]       + thisCoupon)/previousBids[pid]
-                thisReturn  = (currentBids[pid] + thisCoupon)/previousMid
-                numPositions += 1
-                if debug:
-                    print("      SELL ProductId:",pid,"Return:",thisReturn,"PreviousMid:",previousMid,"CurrentBid:",currentBids[pid],"Coupons:",thisCoupon)
-            elif pos>0:
-                # reference BIDs only
-                # thisReturn  = (currentBids[pid]       + thisCoupon)/previousBids[pid]
-                thisReturn  = (currentMid       + thisCoupon)/previousMid
-                numPositions += 1
-                if debug:
-                    print("         ProductId:",pid,"Return:",thisReturn,"PreviousMid:",previousMid,"CurrentMid:",currentMid,"Coupons:",thisCoupon)
-            else:
-                thisReturn  = 0.0
+                previousWeight = previousWeights[pid]
+            weightChanges[pid]   = currentWeights[pid] - previousWeight
+            absSumWeightChanges += abs(weightChanges[pid])
+
+        #               indexValue = sumOverProductId(previousUnits*previousMids)
+        thisIndexValue = 0.0
+        for pid,pos in previousUnits.items():
+            previousMid      = (previousBids[pid] + previousAsks[pid])/2.0
+            thisIndexValue  += previousUnits[pid] * previousMid
+        if isFirstDate:
+            indexValue = 1000.0
+        else:
+            indexValue = thisIndexValue
 
 
-            portReturn  += thisReturn                #  previousDate >= datetime.date(2015,4,21)
+        #  3.2 IF absSum(WeightChanges) != 0.0   rebalance at t-1
+        if absSumWeightChanges != 0.0:
+            for pid,pos in currentWeights.items():
+                previousMid       = (previousBids[pid] + previousAsks[pid])/2.0
+                currentUnits[pid] = indexValue * pos / previousMid  # trade at MIDs, and record transaction costs below in cashflows
+                if pid not in previousUnits:
+                    previousUnit = 0.0
+                else:
+                    previousUnit = previousUnits[pid]
+                unitsChange       = currentUnits[pid] - previousUnit
+                if unitsChange > 0.0 :
+                    cashflow -= unitsChange * (previousAsks[pid] - previousMid)
+                elif unitsChange < 0.0 :
+                    cashflow -= unitsChange * (previousMid - previousBids[pid])
+
+        #  3.3 handle cashflows
+        slippage      = 1.0 + cashflow/indexValue
+        indexValue    = indexValue * slippage
+        for pid,pos in currentUnits.items():
+            currentUnits[pid] = currentUnits[pid] * slippage
+
         # calc index
         if isFirstDate:
             isFirstDate          = False
         else:
-            portReturn  /= numPositions
-            indexValue  *= portReturn
             updateWeightsString += ","
             updateLevelsString  += ","
 
@@ -266,7 +315,7 @@ for productPrice in productPrices:
         # save new index value
         #
         print("Index:",previousDate,indexValue)
-        weightsString =  "("+format(strategyId)+",20,'"+format(previousDate)+"',"+format(numPositions)+")"
+        weightsString =  "("+format(strategyId)+",20,'"+format(previousDate)+"',"+format(len(currentUnits))+")"
         updateWeightsString += weightsString
         levelsString  =  "("+format(strategyId)+",'"+format(previousDate)+"',"+format(indexValue)+")"
         updateLevelsString  += levelsString
@@ -278,6 +327,8 @@ for productPrice in productPrices:
         # ... track positions
         for pid,pos in currentPositions.items():
             previousPositions[pid] = currentPositions[pid]
+            previousWeights[pid]   = currentWeights[pid]
+            previousUnits[pid]     = currentUnits[pid]
         # ... zero coupons
         for pid,y in sumCoupons.items():
             sumCoupons[pid] = 0.0
@@ -305,12 +356,6 @@ for productPrice in productPrices:
     #
     # for matured products, get maturityPayoff and DateMatured
     #
-
-
-#updateWeightsString += ";"
-#updateLevelsString  += ";"
-#print(updateLevelsString)
-#print(updateWeightsString)
 
 
 # cursor.execute(updateLevelsString,multi=True)
